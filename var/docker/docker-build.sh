46736e982ec1601e3905b05308247a2b79b43099
Lens <45818877+yoanyombapro1234@users.noreply.github.com>
2024-11-16 19:10:46 -0500

    feat: cp
    Signed-off-by: Lens <45818877+yoanyombapro1234@users.noreply.github.com>

--- var/docker/docker-build.sh
+++ var/docker/docker-build.sh
@@ -2,6 +2,6 @@
 
 set -o xtrace
 
-docker rmi localhost/postiz || true
-docker build --target dist -t localhost/postiz -f Dockerfile.dev .
-docker build --target devcontainer -t localhost/postiz-devcontainer -f Dockerfile.dev .
+docker rmi localhost/thinkthankmedia || true
+docker build --target dist -t localhost/thinkthankmedia -f Dockerfile.dev .
+docker build --target devcontainer -t localhost/thinkthankmedia-devcontainer -f Dockerfile.dev .
